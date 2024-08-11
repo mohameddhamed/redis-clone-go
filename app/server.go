6e49b2bfac7b0aa915f1c2c060e294d351cfe83d
mohameddhamed <mohamed.1970.hm@gmail.com>
2024-08-11 15:24:00 +0200

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
+
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
@@ -10,13 +13,57 @@ import (
 func handleConnection(connection net.Conn) {
 	for {
 		buffer := make([]byte, 256)
-		_, err := connection.Read(buffer)
+		// fmt.Println("before", buffer)
+		n, err := connection.Read(buffer)
+
+		// fmt.Println(convert(buffer))
+		// fmt.Println("after", buffer)
+		// _, resp := ReadNextRESP(buffer[:n])
+		// _, resp2 := ReadNextRESP(resp.Data)
+		// fmt.Println(string(resp2.Data))
+		// fmt.Println(string(buffer))
+		// command := string(buffer)
+		// fmt.Println(n, string(buffer[:n]))
+		var arr []string
+		command := string(buffer[:n])
+		// fmt.Print(command)
+		if len(command) > 0 && command[0] == '*' {
+			iterations, _ := strconv.Atoi(string(command[1]))
+			i := 2
+			iter := 0
+			for iter < iterations {
+				// if bulk string ahead
+				// fmt.Println(string(command[i]))
+				if command[i] == '$' {
+					i++
+					bulkLen, _ := strconv.Atoi(string(command[i]))
+					// fmt.Println("bulkLen", bulkLen)
+					i += 2
+					word := command[i : i+bulkLen+1]
+					word = strings.ReplaceAll(word, "\n", "")
+
+					arr = append(arr, word)
+					iter++
+				}
+				i++
+			}
+		} else {
+			arr = append(arr, "1")
+			arr = append(arr, "2")
+		}
+		message := "+PONG\r\n"
+		// fmt.Println(arr)
+		// fmt.Println(arr[0], arr[1])
+		first := strings.ToLower(arr[0])
+		if strings.Contains(first, "echo") {
+			// message = arr[1]
+			message = fmt.Sprintf("$%d\r\n%s\r\n", len(arr[1]), arr[1])
+		}
 
 		if err != nil {
 			os.Exit(1)
 		}
 
-		message := "+PONG\r\n"
 		connection.Write([]byte(message))
 	}
 }
